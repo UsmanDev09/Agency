8a77db2367e8d526289202de7f1801f6584becce
UsmanDev09 <usiddique09@gmail.com>
2024-05-04 20:11:14 +0500

    initialized

---
+++ assets/data/dummydata.js
@@ -0,0 +1,243 @@
+import { AiOutlineCalendar } from "react-icons/ai"
+import { TfiRulerPencil } from "react-icons/tfi"
+import { VscFileSubmodule } from "react-icons/vsc"
+import { BiUser } from "react-icons/bi"
+
+export const home = [
+  {
+    icon: <AiOutlineCalendar size={25} />,
+    title: "We will design a tailored release management strategy for you, aligning your development and operations teams to enable swift and dependable software releases.",
+  },
+  {
+    icon: <TfiRulerPencil size={25} />,
+    title: "Our team will guide you in migrating your applications to cloud environments effectively.",
+  },
+  {
+    icon: <VscFileSubmodule size={25} />,
+    title: "By leveraging Infrastructure as Code (IAC) and adhering to industry-leading practices, we'll oversee your cloud application deployments.",
+  },
+  {
+    icon: <BiUser size={25} />,
+    title: "We will develop a customized Continuous Integration/Continuous Deployment (CI/CD) pipeline to suit your specific requirements.",
+  },
+  {
+    icon: <VscFileSubmodule size={25} />,
+    title: "We will assist in transitioning your monolithic applications to a more efficient microservices architecture.",
+  },
+  {
+    icon: <TfiRulerPencil size={25} />,
+    title: "We will implement a robust system for monitoring, logging, and alerting, ensuring your applications perform at their best",
+  },
+  {
+    icon: <AiOutlineCalendar size={25} />,
+    title: "Our commitment extends to boosting the resilience, availability, scalability, and self-healing features of your applications",
+  },
+  {
+    icon: <BiUser size={25} />,
+    title: "We offer auditing and optimization services for your existing infrastructure.",
+  },
+  {
+    icon: <VscFileSubmodule size={25} />,
+    title: "We will implement security scanners within your application and its development pipelines",
+  },
+]
+export const expertise = [
+  {
+    id: 1,
+    title: "GitOps",
+    cover: "../images/e1.svg",
+    desc: [{ text: "Web design" }, { text: "Web development" }, { text: "Custom icons & illustrations" }, { text: "Hosting" }, { text: "Website audit" }],
+  },
+  {
+    id: 2,
+    title: "CloudOps",
+    cover: "../images/e2.png",
+    desc: [{ text: "Social media marketing" }, { text: "Marketing campaigns" }, { text: "Markting management" }, { text: "SEO" }],
+  },
+  {
+    id: 3,
+    title: "DevOps",
+    cover: "../images/e3.png",
+    desc: [{ text: "E-Commerce website design" }, { text: "WooCommerce" }, { text: "Content management" }, { text: "Hosting" }],
+  },
+  {
+    id: 4,
+    title: "DevSecOps",
+    cover: "../images/e4.png",
+    desc: [{ text: "Visual identity" }, { text: "Branding for social media" }, { text: "Custom illustrations" }],
+  },
+]
+export const testimonial = [
+  {
+    id: 1,
+    name: "Alexander Black",
+    cover: "../images/e1.jpg",
+    post: "Seven consulting - CEO",
+    desc: "Nunc fermentum - tempus erat ligula, sit amet lacinia justo cursus ac. Suspendisse quis nulla tincidunt! Lorem ipsum dolor amet at ornare ex, quis fringilla tortor! Nunc consectetur feugiat rutrum. Sed rhoncus sapien!",
+  },
+  {
+    id: 2,
+    name: "Diana Green",
+    cover: "../images/e2.jpg",
+    post: "Seven Arts - marketing manager",
+    desc: "Cras at ornare fermentum quam et tortor euismod, vel maximus metus tristique at ornare ex, quis fringilla tortor. Aenean semper neque quis consectetur lobortis. Quisque nec convallis ex. Aenean ut metus et nunc cursus aliquet.",
+  },
+  {
+    id: 3,
+    name: "Alexander Black",
+    cover: "../images/e3.jpg",
+    post: "Seven consulting - CEO",
+    desc: "Nunc fermentum - tempus erat ligula, sit amet lacinia justo cursus ac. Suspendisse quis nulla tincidunt! Lorem ipsum dolor amet at ornare ex, quis fringilla tortor! Nunc consectetur feugiat rutrum. Sed rhoncus sapien!",
+  },
+  {
+    id: 4,
+    name: "Diana Green",
+    cover: "../images/e4.jpg",
+    post: "Seven Arts - marketing manager",
+    desc: "Cras at ornare fermentum quam et tortor euismod, vel maximus metus tristique at ornare ex, quis fringilla tortor. Aenean semper neque quis consectetur lobortis. Quisque nec convallis ex. Aenean ut metus et nunc cursus aliquet.",
+  },
+]
+export const showcase = [
+  {
+    id: 1,
+    title: "Neon Lights",
+    cover: "../images/s1.jpg",
+    catgeory: "DIGITAL MARKETING",
+  },
+  {
+    id: 2,
+    title: "Futuristic Furniture",
+    catgeory: "WEBSITES",
+    cover: "../images/s2.jpg",
+  },
+  {
+    id: 3,
+    title: "Smart Living",
+    cover: "../images/s3.jpg",
+    catgeory: "WEBSITES",
+  },
+  {
+    id: 4,
+    title: "Light Painting",
+    cover: "../images/s4.jpg",
+    catgeory: "BRANDING	",
+  },
+  {
+    id: 5,
+    title: "Ideabox",
+    cover: "../images/s5.jpg",
+    catgeory: "BRANDING	",
+  },
+  {
+    id: 5,
+    title: "VR Experience",
+    cover: "../images/s6.jpg",
+    catgeory: "WEBSITES	",
+  },
+]
+export const brand = [
+  {
+    id: 1,
+    cover: "../images/l1.svg",
+  },
+  {
+    id: 2,
+    cover: "../images/l2.svg",
+  },
+  {
+    id: 3,
+    cover: "../images/l3.svg",
+  },
+  {
+    id: 4,
+    cover: "../images/l4.svg",
+  },
+  {
+    id: 5,
+    cover: "../images/l5.svg",
+  },
+  {
+    id: 6,
+    cover: "../images/l6.svg",
+  },
+]
+export const blogdata = [
+  {
+    id: 1,
+    title: "Ligula vel urna accumsan placerat",
+    cover: "../images/b1.webp",
+    catgeory: "INDUSTRY",
+    date: "JANUARY 12, 2023",
+  },
+  {
+    id: 2,
+    title: "Don’t underestimate the lorem ipsum dolor amet",
+    cover: "../images/b2.jpg",
+    catgeory: "TIPS & TRICKS",
+    date: "OCTOBER 20, 2023",
+  },
+  {
+    id: 3,
+    title: "Building the real VR lorem ipsum dolor amet glavrida from a scratch",
+    cover: "../images/b3.jpg",
+    catgeory: "TIPS & TRICKS",
+    date: "OCTOBER 9, 2023",
+  },
+  {
+    id: 4,
+    title: "What eleifend posuere tincidunt",
+    cover: "../images/b4.jpg",
+    catgeory: "EVENTS",
+    date: "OCTOBER 8, 2023",
+  },
+]
+export const teamdata = [
+  {
+    id: 1,
+    title: "Alexander Black",
+    cover: "../images/t1.jpg",
+    post: "FOUNDER, CEO",
+  },
+  {
+    id: 2,
+    title: "Anna Kovalenko",
+    cover: "../images/t2.jpg",
+    post: "FINANCE DIRECTOR",
+  },
+  {
+    id: 3,
+    title: "Tiffany White",
+    cover: "../images/t3.jpg",
+    post: "CREATIVE DIRECTOR",
+  },
+  {
+    id: 4,
+    title: "Richard Greenwood",
+    cover: "../images/t4.jpg",
+    post: "PROGRAMMER",
+  },
+  {
+    id: 5,
+    title: "Jessica Brown",
+    cover: "../images/t5.jpg",
+    post: "MARKETING DIRECTOR",
+  },
+  {
+    id: 6,
+    title: "Gregory Windstorm",
+    cover: "../images/t6.jpg",
+    post: "ACCOUNTING MANAGER",
+  },
+  {
+    id: 7,
+    title: "Anna Red",
+    cover: "../images/t7.jpg",
+    post: "PROJECT MANAGER",
+  },
+  {
+    id: 8,
+    title: "Join our team!",
+    cover: "../images/t8.jpg",
+    post: "",
+  },
+]
---
+++ sections/Hero.js
@@ -0,0 +1,64 @@
+import { home } from "@/assets/data/dummydata"
+import Banner from "@/components/Banner"
+import Expertise from "@/components/Expertise"
+import ShowCase from "@/components/ShowCase"
+import Testimonial from "@/components/Testimonial"
+import { Title, TitleLogo, TitleSm } from "@/components/common/Title"
+import { BlogCard, Brand } from "@/components/router"
+import React from "react"
+
+const Hero = () => {
+  return (
+    <>
+      <section className='hero'>
+        <div className='container'>
+          {/* <TitleLogo title='creative' caption='7' className='logobg' /> */}
+          <h1 className='hero-title'>WE PROVIDE DEVOPS SERVICES</h1>
+
+          <div className='sub-heading'>
+            <TitleSm title='GitOps' /> <span>.</span>
+            <TitleSm title='CloudOps' /> <span>.</span>
+            <TitleSm title='DevOps' />  <span>.</span>
+            <TitleSm title='DevSecOps' /> <span>.</span>
+            <TitleSm title='FinOps' /> 
+          </div>
+        </div>
+      </section>
+      <section className='hero-sec'>
+        <div className='container'>
+          <div className='heading-title'>
+            <Title title='The last agency you ll ever need' />
+            <p>Managing a DevOps infrastructure comes with its set of challenges, from the swift
+evolution of technology to the intricacies of system integrations and escalating
+demands that can burden your team. Our DevOps Consulting services for cloud
+solutions provide seamless maintenance, boundless scalability, and enhancedperformance. Whether it's migrating to innovative platforms, adding new technology
+layers, conducting audits, establishing continuous integration and continuous
+deployment (CI/CD), monitoring, or securing your existing and future environments, we
+are dedicated to fulfilling all your DevOps consulting needs.</p>
+          </div>
+          <div className='hero-content grid-4 '>
+            {home.map((item, i) => (
+              <div className='box' key={i}>
+                <span className='green'>{item.icon}</span> <br />
+                <br />
+                <h3>{item.title}</h3>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      <Expertise />
+      <Banner />
+      {/* <Testimonial /> */}
+      {/* <ShowCase /> */}
+      {/* <Brand /> */}
+
+      {/* <div className='text-center'>
+        <Title title='Latest news & articles' />
+      </div>
+      <BlogCard /> */}
+    </>
+  )
+}
+
+export default Hero
